9a96ced485f7a468d2bc9ce2966ce7f7e2ed085b
Elmira <154009217+future-genius16@users.noreply.github.com>
2025-10-30 14:23:58 +0300

    Create CEcoLab2.h

---
+++ HeaderFiles/CEcoLab2.h
@@ -0,0 +1,21 @@
+#ifndef __C_ECOLAB2_H__
+#define __C_ECOLAB2_H__
+
+#include "IEcoLab2.h"
+#include "IEcoCalculatorX.h"
+#include "IEcoLab1.h"
+
+typedef struct CEcoLab2 {
+    IEcoLab2VTbl* pVTbl;
+    uint32_t m_cRef;
+    IEcoUnknownVTbl* m_pISys;
+    IEcoUnknownVTbl* m_pOuterUnknown;
+
+    IEcoCalculatorX* m_pIX;
+    IEcoLab1* m_pILab1;
+} CEcoLab2;
+
+int16_t createInnerComponents(CEcoLab2* me, IEcoInterfaceBus1* pIMgr);
+void demo(CEcoLab2* me);
+
+#endif
